a3c1ecf8b6549f14629a9a974d8caaad27cce722
PranavRao18 <prnvraosrs@gmail.com>
2024-02-12 23:45:00 +0530

    setup

---
+++ src/Sections/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='min-h-screen w-screen flex justify-center items-center flex-col'>
+      <p>Hello There, <span>I'm</span></p>
+      <p>PRANAV RAO</p>
+    </div>
+  )
+}
+
+export default Home
